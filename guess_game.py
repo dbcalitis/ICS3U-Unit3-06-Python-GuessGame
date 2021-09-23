011479f7a8a535ab26794fc2b4d7c48003e1c4d4
Daria Bernice <daria.bernice.calitis@mths.ca>
2021-09-23 16:20:52 +0000

    added guess_game.py

---
+++ guess_game.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+# Created by: Daria Bernice Calitis
+# Created on: Sept 2021
+# This program is the number guessing game
+
+import random
+
+
+def main():
+    # This function is the number guessing game
+    answer = random.randint(0, 9)
+    guess_number = input("Enter a number as your guess (0-9): ")
+    try:
+        # input
+        guess_number = int(guess_number)
+
+        # process & output
+        if guess_number == answer:
+            print("You guessed correctly!")
+        else:
+            print("You guessed incorrectly.")
+
+        print("The correct answer is {0}.".format(answer))
+    except Exception:
+        print("Invalid Input.")
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
